ba982f6cd53df9ac4fc52f44a57d861f55006ccb
BL-Jotiram <bandgar09@gmail.com>
2022-05-16 09:39:38 +0530

    Added functionality to accept value from command line arguments

--- Calculation.java
+++ Calculation.java
@@ -1,8 +1,12 @@
+
 public class Calculation {
 	public static void main(String[] args) {
-		additionOfNum(2, 3);
-		additionOfNum(9, 7);
-		additionOfNum(9, 10);
+		for(int i = 0; i < args.length; i++){
+			System.out.println("Index " + i + " => " + args[i]);
+		}
+		additionOfNum(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+		additionOfNum(Integer.parseInt(args[2]), Integer.parseInt(args[3]));
+		additionOfNum(Integer.parseInt(args[4]), Integer.parseInt(args[5]));
 	}
 
 	public static void additionOfNum(int num1, int num2) {
