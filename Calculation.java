9b60baa34da46df4fc5260a8247bb1a4379865c0
BL-Jotiram <bandgar09@gmail.com>
2022-05-16 09:10:50 +0530

    Added functionality to perform addition of two number

---
+++ Calculation.java
@@ -0,0 +1,12 @@
+public class Calculation {
+	public static void main(String[] args) {
+		additionOfNum(2, 3);
+		additionOfNum(9, 7);
+		additionOfNum(9, 10);
+	}
+
+	public static void additionOfNum(int num1, int num2) {
+		int result = num1 + num2;
+    System.out.println("Result====>" + result);
+	}
+}
